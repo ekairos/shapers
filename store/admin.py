192bb34b309316fdfc7a5713226078299c36f14b
Gareth <gareth.gs13@gmail.com>
2020-08-09 21:28:40 +0200

    Added Category and Product models + tests; installed Pillow

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    readonly_fields = ('sku',)
+
+
+admin.site.register(Category)
+admin.site.register(Product)
--- store/models.py
+++ store/models.py
@@ -1,3 +1,70 @@
 from django.db import models
+from uuid import uuid4
+from django.core.exceptions import ValidationError
 
-# Create your models here.
+
+def new_image_path(instance, filename):
+    """
+    Rename image file and upload to
+    'media/products/<product_sku>/<new_file_name>.<file_extension>'
+
+    :param instance: the instance model name
+    :param filename: name of actual file uploaded
+    :return: String : <path>/<filename>.<extension>
+    """
+
+    file_extension = filename.rsplit('.', 1)[-1].lower()
+
+    file_name = f'image_01.{file_extension}'
+    path = f'products/{instance.sku}'
+
+    return f'{path}/{file_name}'
+
+
+class Category(models.Model):
+    """Simple Category model for Products"""
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=28)
+    description = models.CharField(max_length=160, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.name or self.name.isspace():
+            raise ValidationError('Missing name')
+        else:
+            super().save(*args, **kwargs)
+
+
+class Product(models.Model):
+    """Products model for Store and Partners users"""
+
+    class Meta:
+        ordering = ('-date_version', 'name')
+
+    name = models.CharField(max_length=80)
+    sku = models.UUIDField(default=uuid4, editable=False)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL,
+                                 null=True, blank=True)
+    description = models.TextField(max_length=999)
+    date_version = models.DateField(auto_now_add=True)
+    base_price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(upload_to=new_image_path)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.name or self.name.isspace():
+            raise ValidationError('Missing name')
+        elif not self.description or self.description.isspace():
+            raise ValidationError({'description': 'Missing description'})
+        elif not self.base_price or self.base_price < 0.1:
+            raise ValidationError(
+                {'base_price': 'Please set a minimum price of 0.1'})
+        else:
+            super().save(*args, **kwargs)
