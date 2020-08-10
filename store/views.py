3e959312ebe9fb8977a8e99865ae20157e893ce5
Gareth <gareth.gs13@gmail.com>
2020-08-10 11:02:11 +0200

    Category browsing, updated sidenav links

--- store/views.py
+++ store/views.py
@@ -7,6 +7,11 @@ def store(request):
 
     products = Product.objects.all()
 
+    if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+
     context = {
         'products': products
     }
