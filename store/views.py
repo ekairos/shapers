d517d7e0620a9163f7191b22d267c16122890a3c
Gareth <gareth.gs13@gmail.com>
2020-08-10 18:28:50 +0200

    Keyword Search; added search tool bar, updated store view and template

--- store/views.py
+++ store/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product
+from django.db.models import Q
 
 
 def store(request):
@@ -7,13 +8,22 @@ def store(request):
 
     products = Product.objects.all()
 
+    query_string = None
+
     if request.GET:
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
 
+        if 'q' in request.GET:
+            query_string = request.GET['q']
+            queries = Q(name__icontains=query_string) | Q(
+                description__icontains=query_string)
+            products = products.filter(queries)
+
     context = {
-        'products': products
+        'products': products,
+        'query_string': query_string,
     }
 
     return render(request, 'store/store.html', context=context)
