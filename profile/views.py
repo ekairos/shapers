23f60139568ba603a3a115e0b77852b47fac94ba
Gareth <gareth.gs13@gmail.com>
2020-08-15 15:58:47 +0200

    Updated Profile template & view with user data

--- profile/views.py
+++ profile/views.py
@@ -1,9 +1,16 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import UserProfile
 
 
 @login_required
 def profile(request):
     """View returning Profile page"""
 
-    return render(request, 'profile/profile.html')
+    user_profile = get_object_or_404(UserProfile, user=request.user)
+
+    context = {
+        'user_profile': user_profile
+    }
+
+    return render(request, 'profile/profile.html', context=context)
