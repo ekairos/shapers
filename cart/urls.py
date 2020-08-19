cb0ebb09dd053385ff20b2c0fb852a385392ad29
Gareth <gareth.gs13@gmail.com>
2020-08-19 13:09:09 +0200

    Updated Cart template, views & urls; Added +/- quantity per cart product

--- cart/urls.py
+++ cart/urls.py
@@ -7,5 +7,9 @@ urlpatterns = [
     path('add/<str:product_id>/', views.add_to_cart, name='add_to_cart'),
     path('remove/<str:product_id>/', views.remove_from_cart,
          name='remove_from_cart'),
-    path('update/<str:product_id>/', views.update_cart, name='update_cart')
+    path('update/<str:product_id>/', views.update_cart, name='update_cart'),
+    path('decrement_cart/<str:product_id>/', views.decrement_cart_product,
+         name='decrement_cart_product'),
+    path('increment_cart/<str:product_id>/', views.increment_cart_product,
+         name='increment_cart_product'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -13,53 +13,103 @@ def get_cart(request):
 def add_to_cart(request, product_id):
     """View adding a given product to the Cart"""
 
+    redirect_to = request.POST.get('redirect_url')
     product = get_object_or_404(Product, pk=product_id)
     quantity = int(request.POST.get('quantity'))
-    redirect_to = request.POST.get('redirect_url')
 
-    messages.success(request, f"Added {quantity} {product} to your cart")
+    try:
 
-    cart = request.session.get('cart', {})
+        messages.success(request, f"Added {quantity} {product} to your cart")
 
-    if product_id in cart.keys():
-        cart[product_id] = cart[product_id] + quantity
-    else:
-        cart[product_id] = quantity
+        cart = request.session.get('cart', {})
 
-    request.session['cart'] = cart
+        if product_id in cart.keys():
+            cart[product_id] = cart[product_id] + quantity
+        else:
+            cart[product_id] = quantity
 
+        request.session['cart'] = cart
+
+    except Exception:
+        messages.error(request, f'Sorry, couldn\'t add {product}'
+                                f'to your cart.')
     return redirect(redirect_to)
 
 
 def remove_from_cart(request, product_id):
-    """View removing a Product from the cart"""
+    """View removing a product from the cart"""
 
     cart = request.session.get('cart', {})
     product = get_object_or_404(Product, pk=product_id)
 
-    cart.pop(product_id)
-    request.session['cart'] = cart
+    try:
+        cart.pop(product_id)
+        request.session['cart'] = cart
 
-    messages.success(request, f"Removed {product} from your cart")
-
+        messages.success(request, f"Removed {product} from your cart")
+    except Exception:
+        messages.error(request, f'Sorry, we couldn\'t remove {product}'
+                                f'from your cart.')
     return redirect(reverse('get_cart'))
 
 
 def update_cart(request, product_id):
-    """View updating the quantity of a quantity in the cart"""
+    """View updating the quantity of a given product in the cart"""
 
     product = get_object_or_404(Product, pk=product_id)
     quantity = int(request.POST.get('quantity'))
     cart = request.session.get('cart')
 
-    if quantity > 0:
-        cart[product_id] = quantity
-        messages.success(request, f"You have now {quantity} of {product} "
-                                  f"in your cart")
-    else:
-        cart.pop(product_id)
-        messages.success(request, f"Removed {product} from your cart")
+    try:
+        if quantity > 0:
+            cart[product_id] = quantity
+            request.session['cart'] = cart
+            messages.success(request, f"You have now {quantity} of {product} "
+                                      f"in your cart")
+        else:
+            cart.pop(product_id)
+            messages.success(request, f"Removed {product} from your cart")
 
-    request.session['cart'] = cart
+    except Exception:
+        messages.error(request, f'Error while updating your {product}')
 
+    return redirect(reverse('get_cart'))
+
+
+def decrement_cart_product(request, product_id):
+    """View decrementing the quantity of a given product in the cart"""
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    try:
+        cart = request.session.get('cart')
+
+        if cart[product_id] > 1:
+            cart[product_id] -= 1
+            request.session['cart'] = cart
+            messages.success(request, f"You have now {cart[product_id]} of "
+                                      f"{product} in your cart")
+        else:
+            cart.pop(product_id)
+            request.session['cart'] = cart
+            messages.success(request, f"{product} has been removed from "
+                                      f"your cart")
+    except Exception:
+        messages.error(request, f'Error while updating your {product}')
+    return redirect(reverse('get_cart'))
+
+
+def increment_cart_product(request, product_id):
+    """View incrementing the quantity of the given product in the cart"""
+
+    product = get_object_or_404(Product, pk=product_id)
+    try:
+        cart = request.session.get('cart')
+
+        cart[product_id] += 1
+        request.session['cart'] = cart
+        messages.success(request, f"You have now {cart[product_id]} of "
+                                  f"{product} in your cart")
+    except Exception:
+        messages.error(request, f'Error while updating your {product}')
     return redirect(reverse('get_cart'))
