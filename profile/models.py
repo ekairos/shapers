a752d0420dd0bdca8c6d7834f73e1975e330afe5
Gareth <gareth.gs13@gmail.com>
2020-08-26 23:35:25 +0200

    Updated Profile form, placeholders, required fields + Countryfield

--- profile/models.py
+++ profile/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.dispatch import receiver
 from django.db.models.signals import post_save
+from django_countries.fields import CountryField
 
 
 class UserProfile(models.Model):
@@ -17,7 +18,8 @@ class UserProfile(models.Model):
     profile_postcode = models.CharField(max_length=20, null=True, blank=True)
     profile_town_or_city = models.CharField(max_length=40, null=True,
                                             blank=True)
-    profile_country = models.CharField(max_length=40, null=True, blank=True)
+    profile_country = CountryField(blank_label='Country',
+                                   null=True, blank=True)
 
     def __str__(self):
         if self.user.first_name and self.user.last_name:
