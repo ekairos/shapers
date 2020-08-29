6b0773494ef23c6f29e2d0c03ed77f85f6e59a72
Gareth <gareth.gs13@gmail.com>
2020-08-29 08:53:28 +0200

    Added 3D File model for Products; Updated Products admin;

--- store/admin.py
+++ store/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
-from .models import Category, Product
+from .models import Category, Product, Product3DFile
+
+
+class Product3DFileAdminInline(admin.TabularInline):
+    model = Product3DFile
+    max_num = 1
 
 
 class ProductAdmin(admin.ModelAdmin):
     readonly_fields = ('sku',)
+    inlines = (Product3DFileAdminInline,)
 
 
 admin.site.register(Category)
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
--- store/models.py
+++ store/models.py
@@ -3,7 +3,7 @@ from uuid import uuid4
 from django.core.exceptions import ValidationError
 
 
-def new_image_path(instance, filename):
+def _new_image_path(instance, filename):
     """
     Rename image file and upload to
     'media/products/<product_sku>/<new_file_name>.<file_extension>'
@@ -17,6 +17,24 @@ def new_image_path(instance, filename):
 
     file_name = f'image_01.{file_extension}'
     path = f'products/{instance.sku}'
+
+    return f'{path}/{file_name}'
+
+
+def _new_product_files_path(instance, filename):
+    """
+    Rename product image's file to
+    'media/products/<product_name>/<uuid4_random_number>.<file_extension>'
+
+    :param instance: the instance model name
+    :param filename: name of actual file uploaded
+    :return: String : <path>/<filename>.<extension>
+    """
+
+    file_extension = filename.rsplit('.', 1)[-1].lower()
+
+    file_name = f'{uuid4().hex}.{file_extension}'
+    path = f'products/{instance.product.sku}'
 
     return f'{path}/{file_name}'
 
@@ -53,7 +71,7 @@ class Product(models.Model):
     description = models.TextField(max_length=999)
     date_version = models.DateField(auto_now_add=True)
     base_price = models.DecimalField(max_digits=6, decimal_places=2)
-    image = models.ImageField(upload_to=new_image_path)
+    image = models.ImageField(upload_to=_new_image_path)
 
     def __str__(self):
         return self.name
@@ -68,3 +86,16 @@ class Product(models.Model):
                 {'base_price': 'Please set a minimum price of 0.1'})
         else:
             super().save(*args, **kwargs)
+
+
+class Product3DFile(models.Model):
+    """Model for Products 3D files"""
+
+    product = models.OneToOneField(Product, on_delete=models.SET_NULL,
+                                   related_name='printing_file',
+                                   null=True, blank=True)
+    file = models.FileField(upload_to=_new_product_files_path,
+                            null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.product.name} \'s printing file'
